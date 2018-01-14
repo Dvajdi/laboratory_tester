db787dc06d3b5a81ca79b72d09a45af7d6366adb
Twice <dvajdi@bk.ru>
2018-01-14 11:24:03 +0300

    WIP

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -21,6 +21,7 @@ const routes:Routes = [
     CentrifugeComponent
   ],
   imports: [
+    FormsModule,
     BrowserModule,
     RouterModule.forRoot(routes)
   ],
--- src/app/centrifuge/centrifuge.component.ts
+++ src/app/centrifuge/centrifuge.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-centrifuge',
@@ -7,9 +7,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CentrifugeComponent implements OnInit {
 
-  constructor() { }
+  radius;
+  frequency;
+  acceleration;
+
+  constructor() {
+  }
 
   ngOnInit() {
   }
 
+  execute() {
+    console.log(this.radius);
+    console.log(this.frequency);
+    console.log(this.acceleration);
+  }
 }
